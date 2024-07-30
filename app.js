646810acf88ee335a31a35038559a05f10ca7a36
Mazharrais <mazharmajo72@gmail.com>
2024-07-30 23:07:47 +0500

    add code

--- app.js
+++ app.js
@@ -1,10 +1,55 @@
 
 
+
 var screen = document.getElementById("display");
 
+// let num; 
 
-function pressBtn(){
-    
+var screenSet;
+var operator;
+
+function pressBtn(num){
+    if(screenSet){
+    screen.value = num;
+    screenSet = false;
+}
+else{
+    screen.value += num;
+}
+   
+}
+
+function setOperator(ope){
+
+    operator = ope;
+    console.log(operator);
+    screenSet = true;
+    result = Number(screen.value);
+    console.log(result);
+
+
+}
+
+function getResult(){
+    switch (operator){
+    case '+' : 
+    screen.value = result + Number(screen.value);
     console.log(screen.value);
-    
+    break;
+    case '-' : 
+    screen.value = result - Number(screen.value);
+    break;
+    case '*' : 
+    screen.value = result * Number(screen.value);
+    break;
+    case '/' : 
+    screen.value = result / Number(screen.value);
+    break;
+    }
+}
+
+
+function clrAll(){
+screen.value = "";
+
 }
